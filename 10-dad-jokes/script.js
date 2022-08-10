4e8bc1fc46dcb036d826372edc50479f576432f3
skjoeldkrona <skjoeldkrona@gmail.com>
2022-08-10 17:02:41 +0200

    feat: Fetching jokes with fetch completed

--- 10-dad-jokes/script.js
+++ 10-dad-jokes/script.js
@@ -1,1 +1,18 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
 
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: { Accept: 'application/json' },
+  };
+
+  fetch('https://icanhazdadjoke.com/', config)
+    .then((response) => response.json())
+    .then((data) => {
+      jokeElement.innerText = data.joke;
+    });
+}
